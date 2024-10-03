caea7033d7297addbcb5b82dd244402435220fe8
unknown <114921992+dl960@users.noreply.github.com>
2024-10-03 17:18:41 +0100

    Added Week2

--- Lectures/W2L2 examples/randomColours/sketch.js
+++ Lectures/W2L2 examples/randomColours/sketch.js
@@ -13,10 +13,4 @@ function draw() {
     background(0);
     fill(redChannel, greenChannel, blueChannel);
     circle(width / 2, height / 2, width * 0.8);
-    // r++;
-    // r %= 255;
-    // g++;
-    // g %= 255;
-    // b++;
-    // b %= 255;
 }
